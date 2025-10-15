e94367e3f13e0839ddc077f5b753a8aadc4c2cbd
Tsitsimutsvedu <mutsvedutsitsi@gmail.com>
2025-10-15 16:32:22 +0200

    week07 team project

--- middleware/userValidation.js
+++ middleware/userValidation.js
@@ -1,16 +1,43 @@
 const { param, body } = require('express-validator');
 
+// Validate MongoDB ObjectId in route parameter
 exports.validateUserId = [
-  param('id').isMongoId().withMessage('Invalid user ID'),
+  param('id')
+    .isMongoId()
+    .withMessage('Invalid user ID'),
 ];
 
+// Validate user body fields
 exports.validateUserBody = [
-  body('name').notEmpty().withMessage('Name is required'),
-  body('email').isEmail().withMessage('Valid email is required'),
-  body('roles').optional().isArray().withMessage('Roles must be an array'),
+  body('name')
+    .trim()
+    .notEmpty()
+    .withMessage('Name is required'),
+
+  body('email')
+    .isEmail()
+    .withMessage('Valid email is required'),
+
+  body('roles')
+    .optional()
+    .isArray()
+    .withMessage('Roles must be an array'),
+
   body('googleId')
     .optional()
     .isString()
     .withMessage('Google ID must be a string'),
-  //   body('profilePicture').optional().isURL().withMessage('Profile picture must be a valid URL'),
+
+  // Avoid using isURL due to known vulnerability
+  body('profilePicture')
+    .optional()
+    .custom((value) => {
+      try {
+        new URL(value);
+        return true;
+      } catch {
+        throw new Error('Profile picture must be a valid URL');
+      }
+    }),
 ];
+
